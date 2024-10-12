1238c76c8f449b1738bcdccf384c22bc10019217
Derek <78566663+dvorakman@users.noreply.github.com>
2024-10-12 20:09:47 +1100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -10,55 +10,117 @@
       ./hardware-configuration.nix
     ];
 
+  # fileSystem Specfications
+  fileSystems = {
+    "/".options = [ "compress=zstd" "noatime" "ssd" "space_cache=v2" "autodefrag" "discard=async" ];
+    "/root".options = [ "compress=zstd" "noatime" "ssd" "space_cache=v2" "autodefrag" "discard=async" ];
+    "/home".options = [ "compress=zstd" "noatime" "ssd" "space_cache=v2" "autodefrag" "discard=async" ];
+    "/nix".options = [ "compress=zstd" "noatime" "ssd" "space_cache=v2" "autodefrag" "discard=async" ];
+  };
+
+  # btrfs specific options
+  services.btrfs.autoScrub.enable = {
+    enable = true;
+    interval = "monthly";
+    fileSystems = [ "/" ];
+  }; 
+ 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Australia/Sydney";
-  
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_AU.UTF-8";
   console = {
     font = "Lat2-Terminus16";
-    keyMap = lib.mkForce "dvorak";
+    keyMap = "dvorak";
+    useXkbConfig = true; # use xkb.options in tty.
   };
 
-  programs.hyprland.enable = true; # enable Hyprland
-  environment.sessionVariables.NIXOS_OZONE_WL = "1"; # hint Electron apps to use Wayland
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+
+  # Enable the GNOME Desktop Environment.
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
 
   # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
   services.pipewire = {
     enable = true;
     pulse.enable = true;
   };
 
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.cardinal = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    hashedPassword = "$y$j9T$N46AdNaMGDvxiNlcCLb0V1$7OguLKhte.PC6IyUFecFatYpVXbFrEFHTBqYbQBCUy0";
     packages = with pkgs; [
       firefox
+      kitty
+      wofi
+      dolphin
       tree
     ];
   };
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    kitty
+    git
   ];
 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
