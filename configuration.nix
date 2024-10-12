47289b227410a51232860b479394116c9f18e991
Derek <78566663+dvorakman@users.noreply.github.com>
2024-10-12 20:53:35 +1100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,16 @@
     interval = "monthly";
     fileSystems = [ "/" ];
   }; 
+
+  # erase your darlings
+  {
+    boot.initrd.postDeviceCommands = lib.mkAfter ''
+      mkdir /mnt
+      mount -t btrfs /dev/mapper/enc /mnt
+      btrfs subvolume delete /mnt/root
+      btrfs subvolume snapshot /mnt/root-blank /mnt/root
+    '';
+  }
  
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
