11d87103b87055b12249877b0fe929b2a1c4bff3
Derek <78566663+dvorakman@users.noreply.github.com>
2024-11-27 23:43:22 +1100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,10 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager";
-    # Optionally add more inputs like a custom NixOS flake
+    disko.url = "github:nix-community/disko/latest";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, disko, ... }:
     let
       system = "x86_64-linux";  # Adjust for your architecture
     in
@@ -15,8 +16,9 @@
       nixosConfigurations.mySystem = nixpkgs.lib.nixosSystem {
         system = system;
         modules = [
-          ./configuration.nix  # Include your main system configuration
-          ./hardware-configuration.nix  # Include hardware config
+          ./configuration.nix
+          ./hardware-configuration.nix
+          disko.nixosModules.disko
         ];
         specialArgs = { inherit system; };
       };
@@ -24,7 +26,7 @@
       homeConfigurations.cardinal = home-manager.lib.homeManagerConfiguration {
         system = system;
         modules = [
-          ./home.nix  # Include your user-specific configuration here
+          ./home.nix
         ];
       };
     };
